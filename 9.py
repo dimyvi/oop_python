3610e7d71123921e7e0e05753ac14eaf003b898e
dimyvi <dimyvybornov@gmail.com>
2025-07-15 19:21:51 +0300

    new

---
+++ 9.py
@@ -0,0 +1,47 @@
+class Cart:
+    goods = []
+    def add(self, gd):
+        self.goods.append(gd)
+
+    def remove(self, indx):
+        self.goods.remove(self.goods[indx])
+
+    def get_list(self):
+        return [f"{obj.name}:{obj.price}" for obj in self.goods]
+
+class Table:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+class TV:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+class Notebook:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+class Cup:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+
+cart = Cart()
+
+tv1 = TV("samsung", 1111)
+tv2 = TV("LG", 1234)
+table = Table("ikea", 2345)
+n1 = Notebook("msi", 5433)
+n2 = Notebook("apple", 542)
+c = Cup("keepcup", 43)
+
+cart.add(tv1)
+cart.add(tv2)
+cart.add(table)
+cart.add(n1)
+cart.add(n2)
+cart.add(c)
