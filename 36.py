41e51114b9be13a6969e0e9ce7fb01bbd97e19ed
dimyvi <dimyvybornov@gmail.com>
2025-07-29 11:52:26 +0300

    new

---
+++ 36.py
@@ -0,0 +1,50 @@
+class StringValue:
+    def __init__(self, min_length=2, max_length=50):
+        self.min_length = min_length
+        self.max_length = max_length
+
+    def __set_name__(self, owner, name):
+        self.name = name
+
+    def __get__(self, instance, owner):
+        return instance.__dict__.get(self.name)
+
+    def __set__(self, instance, value):
+        if isinstance(value, str) and self.min_length <= len(value) <= self.max_length:
+            instance.__dict__[self.name] = value
+
+
+class PriceValue:
+    def __init__(self, max_value=10000):
+        self.max_value = max_value
+
+    def __set_name__(self, owner, name):
+        self.name = name
+
+    def __get__(self, instance, owner):
+        return instance.__dict__.get(self.name)
+
+    def __set__(self, instance, value):
+        if isinstance(value, (int, float)) and 0 <= value <= self.max_value:
+            instance.__dict__[self.name] = value
+
+
+class SuperShop:
+    def __init__(self, name):
+        self.name = name
+        self.goods = []
+
+    def add_product(self, product):
+        self.goods.append(product)
+
+    def remove_product(self, product):
+        self.goods.remove(product)
+
+
+class Product:
+    name = StringValue()
+    price = PriceValue()
+
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
