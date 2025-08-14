9f2ca3f8979e8fcfe3957f861400d2aa139c3413
dimyvi <dimyvybornov@gmail.com>
2025-08-14 21:43:39 +0300

    new

---
+++ 55.py
@@ -0,0 +1,15 @@
+import sys
+#ctrl+D прервать чтение ввода
+class Book:
+    def __init__(self, title, author, pages):
+        self.title = title
+        self.author = author
+        self.pages = pages
+
+    def __str__(self):
+        return f"Книга: {self.title}; {self.author}; {self.pages}"
+
+lst_in = list(map(str.strip, sys.stdin.readlines()))
+
+book = Book(*lst_in)
+print(book)
