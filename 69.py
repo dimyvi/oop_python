e7327f7c6d68f147a98460655933f180a194c688
dimyvi <dimyvybornov@gmail.com>
2025-08-26 03:31:56 +0300

    new

---
+++ 69.py
@@ -0,0 +1,23 @@
+class Body:
+    def __init__(self, name, ro, volume):
+        self.name = name
+        self.ro = ro
+        self.volume = volume
+
+    def mass(self):
+        return self.ro * self.volume
+
+    def __eq__(self, other):
+        return self.mass() == other
+
+    def __lt__(self, other):
+        return self.mass() < other
+
+    def __gt__(self, other):
+        return self.mass() > other
+
+    def __le__(self, other):
+        return self.mass() <= other
+
+    def __ge__(self, other):
+        return self.mass() >= other
